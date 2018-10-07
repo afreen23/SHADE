a04220d5440b95b4f5d3fcbaca9dced854dcc44e
afreen <afreenrahman78@gmail.com>
2018-10-07 17:53:02 +0530

    fixing

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
 from flask import Flask,redirect,url_for,jsonify,render_template
 from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
 from flask_cors import CORS,cross_origin
-from flask_restful import Resource,Api
+from keys import CONSUMER_SECRET,CONSUMER_SECRET_KEY
+# from flask_restful import Resource,Api
 #from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
 
 app = Flask(__name__)
-api=Api(app)
-app.config['SECRET_KEY'] = 'thissupposedtobesecret'
-
-twitter_blueprint = make_twitter_blueprint(api_key='YecKuKjZfTp6nd2HbEQlTitPh',api_secret='vs7YRFW9bgZPrQaRF02A42eb918jBpWrsXolVY8OX9FztL6Iyf')
+CORS(app)
+twitter_blueprint = make_twitter_blueprint(api_key=CONSUMER_SECRET,api_secret=CONSUMER_SECRET_KEY)
 app.register_blueprint(twitter_blueprint,url_prefix='/login')
 
 #facebook_blueprint = make_facebook_blueprint(client_id=None, client_secret=None, scope=None, redirect_url=None, redirect_to=None, login_url=None, authorized_url=None, session_class=None, backend=None)
 
-class home(Resource):
-    def get(self):
-        return "Success"
+@app.route('/')
+def home(self):
+    return "Success"
 
-class twitterlogin(Resource):
-    def get(self):
-        if not twitter.authorized:
-            return redirect(url_for("twitter.login"))
-        resp = twitter.get("account/settings.json")
-        resp = twitter.get("statuses/user_timeline.json")
-        assert resp.ok
-        return {'message':"You are @{screen_name} on Twitter".format(screen_name=resp.json()["screen_name"])},200
+@app.route('/twitter')
+def get(self):
+    if not twitter.authorized:
+        return redirect(url_for("twitter.login"))
+    resp = twitter.get("account/settings.json")
+    resp = twitter.get("statuses/user_timeline.json")
+    assert resp.ok
+    return {'message':"You are @{screen_name} on Twitter".format(screen_name=resp.json()["screen_name"])},200
 
-api.add_resource(home,'/')
-api.add_resource(twitterlogin,'/twitter')
 
 if __name__ == '__main__':
     app.run(debug=True)
