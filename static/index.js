dd757be60b82773e3c796a689417dd4ccd497ca8
afreen <afreenrahman78@gmail.com>
2018-10-07 18:16:50 +0530

    checking entire app

--- static/index.js
+++ static/index.js
@@ -9,9 +9,9 @@ function handleSignin(){
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
         // Typical action to be performed when the document is ready:
-        console.log(xhttp.response)
+        console.log("fetched")
         }
     };
-    xhttp.open("GET", "http://localhost:5000/twitter", true);
+    xhttp.open("GET", "https://shadeapp.herokuapp.com/twitter", true);
     xhttp.send()
   }
