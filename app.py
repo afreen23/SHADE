6a0b2386e1fb96ddaf501c16b82fc6d863d632e2
afreen <afreenrahman78@gmail.com>
2018-10-07 16:54:19 +0530

    fixing errors

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
-from flask import Flask,redirect,url_for,render_template
+from flask import Flask,redirect,url_for,jsonify,render_template
 from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
-from keys import CONSUMER_KEY, CONSUMER_SECRET_KEY
+from flask_cors import CORS,cross_origin
+from flask_restful import Resource,Api
 #from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
 
 app = Flask(__name__)
+api=Api(app)
 app.config['SECRET_KEY'] = 'thissupposedtobesecret'
 
-twitter_blueprint = make_twitter_blueprint(api_key=CONSUMER_KEY,api_secret=CONSUMER_SECRET_KEY)
+twitter_blueprint = make_twitter_blueprint(api_key='YecKuKjZfTp6nd2HbEQlTitPh',api_secret='vs7YRFW9bgZPrQaRF02A42eb918jBpWrsXolVY8OX9FztL6Iyf')
 app.register_blueprint(twitter_blueprint,url_prefix='/login')
 
 #facebook_blueprint = make_facebook_blueprint(client_id=None, client_secret=None, scope=None, redirect_url=None, redirect_to=None, login_url=None, authorized_url=None, session_class=None, backend=None)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+class home(Resource):
+    def get(self):
+        return "Success"
 
-@app.route("/twitter")
-def twitter_login():
-    if not twitter.authorized:
-        return redirect(url_for("twitter.login"))
-    resp = twitter.get("account/settings.json")
-    assert resp.ok
-    return "You are @{screen_name} on Twitter".format(screen_name=resp.json()["screen_name"])
+class twitterlogin(Resource):
+    def get(self):
+        if not twitter.authorized:
+            return redirect(url_for("twitter.login"))
+        resp = twitter.get("account/settings.json")
+        resp = twitter.get("statuses/user_timeline.json")
+        assert resp.ok
+        return {'message':"You are @{screen_name} on Twitter".format(screen_name=resp.json()["screen_name"])},200
+
+api.add_resource(home,'/')
+api.add_resource(twitterlogin,'/twitter')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
